01d1e785f3f12a8b7f0b2ae911766b0b61efc7aa
breia <breiaswanigan1@gmail.com>
2020-08-05 17:54:51 -0400

    breia added

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let largePets = [
     quantity: 2,
     price: 1200,
     experience: "expert",
+    // img: image,
   },
   {
     type: "European Long-tail Dragon",
@@ -81,22 +82,51 @@ let smallPets = [
 let menuContainer = document.querySelector(".menu");
 
 let largeContainer = document.querySelector(".large");
-
+// const largeImages = new Array();
+// largeImages[0] = new Image();
+// largeImages[0].src = `images/dragon.png`;
 const display = () => {
   largePets.forEach((largePet) => {
+    let newImage = document.createElement("img");
+    // newImage.setAttribute.src(largeImages[i]);
     let card = document.createElement("div");
     card.classList.add("card");
     let typeSection = document.createElement("p");
     typeSection.innerText = largePet.type;
     let quantitySection = document.createElement("p");
-    quantitySection.innerText = largePet.quantity;
+    quantitySection.innerText = `qty: ${largePet.quantity}`;
     let priceSection = document.createElement("p");
-    priceSection.innerText = `¥${largePet.price}`;
+    priceSection.innerText = `price: ¥${largePet.price}`;
     let experienceSection = document.createElement("p");
     experienceSection.innerText = largePet.experience;
-    let largeImage = document.createElement("img");
-    card.append(typeSection, quantitySection, priceSection, experienceSection);
+    card.append(
+      typeSection,
+      quantitySection,
+      priceSection,
+      experienceSection,
+      newImage
+    );
     largeContainer.append(card);
   });
 };
 display();
+
+let smallContainer = document.querySelector(".small");
+const smallDisplay = () => {
+  smallPets.forEach((smallPet) => {
+    let card = document.createElement("div");
+    card.classList.add("card");
+    let typeSection = document.createElement("p");
+    typeSection.innerText = smallPet.type;
+    let quantitySection = document.createElement("p");
+    quantitySection.innerText = `qty: ${smallPet.quantity}`;
+    let priceSection = document.createElement("p");
+    priceSection.innerText = `price: ¥${smallPet.price}`;
+    let experienceSection = document.createElement("p");
+    experienceSection.innerText = smallPet.experience;
+    let smallImage = document.createElement("img");
+    card.append(typeSection, quantitySection, priceSection, experienceSection);
+    smallContainer.append(card);
+  });
+};
+smallDisplay();
